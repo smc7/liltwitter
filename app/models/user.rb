5c866e46e6b7cc468a10ad1cc907eab54f25c3ef
smc7 <f.bajelan@gmail.com>
2017-02-18 09:31:27 +0330

    toy-app + exercises

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 	has_many :microposts
 	validates :email, presence: true
+	validates_format_of :email,:with => /\A[^@\s]+@([^@\s]+\.)+[^@\s]+\z/
   	validates :name, presence: true 
 end
